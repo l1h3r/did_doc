0d079abb6ddfc5546993bb0cfc35aff8509c6f6c
l1h3r <devin.turner09@gmail.com>
2020-11-08 09:58:53 -0800

    Add JwsVerificationKey2020 MethodType

--- src/verification/method_type.rs
+++ src/verification/method_type.rs
@@ -1,5 +1,6 @@
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
 #[non_exhaustive]
 pub enum MethodType {
+  JwsVerificationKey2020,
   Ed25519VerificationKey2018,
 }
