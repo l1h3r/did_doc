d6944acd930447c5d6d2d762fe6245a35cc7e39c
l1h3r <devin.turner09@gmail.com>
2020-11-25 18:38:05 -0800

    Auto-implement some signature traits

--- src/verifiable/traits.rs
+++ src/verifiable/traits.rs
@@ -1,4 +1,5 @@
 use crate::verifiable::Signature;
+use crate::verifiable::SignatureDocument;
 
 pub trait TrySignature {
   fn try_signature(&self) -> Option<&Signature>;
@@ -10,4 +11,31 @@ pub trait TrySignatureMut: TrySignature {
 
 pub trait SetSignature: TrySignatureMut {
   fn set_signature(&mut self, signature: Signature);
+}
+
+impl<T> TrySignature for T
+where
+  T: SignatureDocument,
+{
+  fn try_signature(&self) -> Option<&Signature> {
+    <T as SignatureDocument>::try_signature(self)
+  }
+}
+
+impl<T> TrySignatureMut for T
+where
+  T: SignatureDocument,
+{
+  fn try_signature_mut(&mut self) -> Option<&mut Signature> {
+    <T as SignatureDocument>::try_signature_mut(self)
+  }
+}
+
+impl<T> SetSignature for T
+where
+  T: SignatureDocument,
+{
+  fn set_signature(&mut self, signature: Signature) {
+    <T as SignatureDocument>::set_signature(self, signature)
+  }
 }
